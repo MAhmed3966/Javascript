b46b1d184cbc852e4bc773e3f89c302796e54554
maaq7236 <91117209+maaq7236@users.noreply.github.com>
2022-12-15 15:04:37 +0500

    Code for setting the cookie

---
+++ Set-and-get-Cookie/cookie.js
@@ -0,0 +1,13 @@
+const COOKIE_EXPIRATION_LIMIT_DAY = 90;
+
+//  Set cookie expiration
+
+function setCookie(name = 'transaction_id') {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
